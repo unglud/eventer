6e0cf54d1a0697de9ed0b403edb9ce2a06c619c6
Aleksandr Matrosov <unglud@gmail.com>
2021-05-21 22:39:53 +0200

    validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hi there");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email should be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 12 })
+      .withMessage("Password must be between 4 and 12 characters."),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    const { email, password } = req.body;
+
+    console.log(`Creating a User`, { email, password });
+
+    res.send({});
+  }
+);
 
 export { router as signUpRouter };
