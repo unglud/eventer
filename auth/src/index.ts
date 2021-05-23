9c1a265dd54a8fc3ef399ddbae581294de7a7c29
Aleksandr Matrosov <unglud@gmail.com>
2021-05-23 11:11:20 +0200

    not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -13,6 +14,10 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+})
 
 app.use(errorHandler);
 
