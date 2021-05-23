07dc024732643f74681d4f17e0f21bce8f6d559a
Aleksandr Matrosov <unglud@gmail.com>
2021-05-23 11:01:17 +0200

    error handler refactor

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
-  { message }: Error,
+  err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log(`Something went wrong`, message);
-  res.status(400).send({ message });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  res.status(400).send({ errors: [{ message: "Something went wrong" }] });
 };
