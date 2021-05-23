2be2ea982a8312aaa4f4a8a4dae068ce4b54543a
Aleksandr Matrosov <unglud@gmail.com>
2021-05-23 11:20:51 +0200

    handle async errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import "express-async-errors";
+
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
@@ -15,7 +17,7 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
-app.all('*', () => {
+app.all('*', async () => {
   throw new NotFoundError();
 })
 
