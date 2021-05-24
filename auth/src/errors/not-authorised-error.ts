807927102827a39558e321ae6f2173f5cf865b01
Aleksandr Matrosov <unglud@gmail.com>
2021-05-24 21:01:48 +0200

    auth check middleware

---
+++ auth/src/errors/not-authorised-error.ts
@@ -0,0 +1,14 @@
+import { CustomError } from './custom-error';
+
+export class NotAuthorisedError extends CustomError {
+  statusCode = 401;
+
+  constructor() {
+    super('Not Authorised');
+    Object.setPrototypeOf(this, NotAuthorisedError.prototype);
+  }
+
+  serializeErrors(): { message: string; field?: string }[] {
+    return [{ message: this.message}];
+  }
+}
