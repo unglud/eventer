7fa24a6eabdf743627e012e0e6964e776cd5c2fd
Aleksandr Matrosov <unglud@gmail.com>
2021-05-24 15:02:58 +0200

    set cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
-
 import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
@@ -11,7 +12,9 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({signed:false, secure:true}));
 
 app.use(currentUserRouter);
 app.use(signInRouter);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
-import { BadRequestError } from '../errors/bad-request-error';
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -27,11 +29,21 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError(`Email in use`)
+      throw new BadRequestError(`Email in use`);
     }
 
     const user = User.build({ email, password });
     await user.save();
+
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "asdf"
+    );
+
+    req.session = { jwt: userJwt };
 
     res.status(201).send(user);
   }
