2fbde101757dc4b2a218c045ed0cf26c1d0a678f
Aleksandr Matrosov <unglud@gmail.com>
2021-05-24 21:52:14 +0200

    test signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -2,8 +2,47 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("should return 201 on successful sign up", async () => {
-  return request(app).post("/api/users/signup").send({
-    email: "test@test.com",
-    password: "password",
-  });
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+});
+
+it("should return 400 on invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("should return 400 on invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "p",
+    })
+    .expect(400);
+});
+
+it("should return 400 missing fields", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "password",
+    })
+    .expect(400);
 });
