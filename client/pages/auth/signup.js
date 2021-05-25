f02db289fbee4017b698e43c54b7e08413352983
Aleksandr Matrosov <unglud@gmail.com>
2021-05-25 10:39:17 +0200

    handle form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
+
 export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(`emails,password`, email,password);
+  }
+
   return (
-    <form action="">
+    <form onSubmit={onSubmit}>
       <h1>Sign up</h1>
       <div className="form-group">
         <label htmlFor="">Email</label>
-        <input type="" className="form-control" />
+        <input value={email} onChange={e=>setEmail(e.target.value)} type="" className="form-control" />
       </div>
       <div className="form-group">
         <label htmlFor="">Password</label>
-        <input type="password" className="form-control" />
+        <input value={password} onChange={e=>setPassword(e.target.value)} type="password" className="form-control" />
       </div>
       <button className="btn btn-primary">Sign Up</button>
     </form>
