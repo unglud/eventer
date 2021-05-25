e99d8593cf263f47602a84b22915049b1c837397
Aleksandr Matrosov <unglud@gmail.com>
2021-05-25 10:55:59 +0200

    form validation

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,13 +4,21 @@ import axios from "axios";
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await axios.post("/api/users/signup", { email, password });
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
 
-    console.log(`response.data`, response.data);
+      console.log(`response.data`, response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -34,6 +42,17 @@ export default () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Oops...</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
