af20761e943cc99629627a5eb7725ef565d43635
Aleksandr Matrosov <unglud@gmail.com>
2021-05-25 10:30:14 +0200

    sign up form

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,16 @@
+export default () => {
+  return (
+    <form action="">
+      <h1>Sign up</h1>
+      <div className="form-group">
+        <label htmlFor="">Email</label>
+        <input type="" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="">Password</label>
+        <input type="password" className="form-control" />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
