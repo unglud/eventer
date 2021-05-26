3d7f8df9a8ca7be29538f7039e19c44aa7e5be30
Aleksandr Matrosov <unglud@gmail.com>
2021-05-26 10:33:16 +0200

    app initial props

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,21 @@
 import "bootstrap/dist/css/bootstrap.css";
+import { buildClient } from "../api/build-client";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const App = ({ Component, pageProps, currentUser }) => {
+  return (
+    <>
+      <h1>Header {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+App.getInitialProps = async ({ Component, ctx }) => {
+  const { data } = await buildClient(ctx).get("/api/users/currentuser");
+  const pageProps =
+    (Component.getInitialProps && (await Component.getInitialProps(ctx))) || {};
+
+  return { pageProps, ...data };
 };
 
 export default App;
